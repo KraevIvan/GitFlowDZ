21f2f5d96069234db9fbde59eae7d9cc698cd618
KraevIvan <KraevIvanA@yandex.ru>
2024-02-22 18:16:33 +0300

    add str_func(up string)

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def up_letters(string):
     return string.upper()
+
+
+def up_first_letter_in_words(string):
+    """Возвращает строку, где первые буквы каждого слова заглавные"""
+    return string.title()
